a23d580fd4e712527af0328dd73b192de36a216c
Aryanpatel2001 <patelaryan8044@gmail.com>
2024-06-23 21:55:15 +0530

    create footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Header from "./components/Header";
 import Projects from "./pages/Projects";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <BrowserRouter>
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
