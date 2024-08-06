ef9bc2e6804df0c483d18c67f6543efdc593c980
Aryanpatel2001 <patelaryan8044@gmail.com>
2024-08-06 15:14:31 +0530

    add create post UI page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default function CreatePost() {
-  return <div>CreatePost</div>;
+  return (
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+      <form className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput
+            type="text"
+            placeholder="Title"
+            required
+            id="title"
+            className="flex-1"
+          />
+          <Select>
+            <option value="uncategorized">Select a category</option>
+            <option value="Javascript">Javascript</option>
+            <option value="React.js">React.js</option>
+            <option value="nextJs">Next.js</option>
+          </Select>
+        </div>
+        <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dashed p-3">
+          <FileInput type="file" accept="image/*" />
+          <Button type="button" size="sm" outline>
+            Upload image
+          </Button>
+        </div>
+        <ReactQuill
+          theme="snow"
+          placeholder="Write something..."
+          className="h-72 mb-12"
+          required
+        />
+        <Button type="submit">Publish</Button>
+      </form>
+    </div>
+  );
 }
