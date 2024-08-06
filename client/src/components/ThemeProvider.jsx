78ad4c9739744ad068c1c4489e186cefc5a8e803
Aryanpatel2001 <patelaryan8044@gmail.com>
2024-08-06 23:05:41 +0530

    add delete post functionality

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,11 +1,13 @@
-import React,  from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
-export default function ThemeProvider({children}) {
+export default function ThemeProvider({ children }) {
   const { theme } = useSelector((state) => state.theme);
-  return <div className={theme}>
-    <div className="bg-white text-gray-700 dark:text-gray-700 dark:bg-[rgb(16,23,42)] min-h-screen">
+  return (
+    <div className={theme}>
+      <div className="bg-white text-gray-700 dark:text-gray-700 dark:bg-[rgb(16,23,42)] min-h-screen">
         {children}
+      </div>
     </div>
-  </div>;
+  );
 }
