1444ac1e7ae702ddf251a1a32364454ee304d004
Aryanpatel2001 <patelaryan8044@gmail.com>
2024-08-06 17:12:15 +0530

    complete upload post image functionality

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 // import bodyParser from "body-parser";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
 import jwt from "jsonwebtoken";
 import User from "./model/User.model.js";
 import cookieParser from "cookie-parser";
@@ -30,6 +31,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
 
 app.post("/forget-password", async (req, res) => {
   const { email } = req.body;
